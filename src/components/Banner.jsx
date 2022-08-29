6ae8e72453aa197445c71a8ae62e9fce843f5427
Anu Kumari <anukumari21529@gmail.com>
2022-08-29 20:03:45 +0530

    style: make banner images responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,50 +1,93 @@
-import { HStack, VStack, Button, Text, Heading, Flex, Stack, Box, Image, Divider, SimpleGrid, GridItem } from '@chakra-ui/react';
-import { BiPlus } from 'react-icons/bi'
+import {
+  HStack,
+  VStack,
+  Button,
+  Text,
+  Heading,
+  Stack,
+  Box,
+  Image,
+} from "@chakra-ui/react";
+import { BiPlus } from "react-icons/bi";
 
-import { bannerData } from '../data';
-import Apartment1Lg from '../assets/images/apartments/a1lg.png';
-import Apartment6Lg from '../assets/images/apartments/a6lg.png';
+import { bannerData } from "../data";
+import Apartment1Lg from "../assets/images/apartments/a1lg.png";
+import Apartment6Lg from "../assets/images/apartments/a6lg.png";
 
 const Banner = () => {
   return (
     <>
-      <Flex>
-
-        <VStack px='8' py='20' bg='pink.100' align='left' borderTopLeftRadius='xl'>
-          <Heading fontSize='3xl'>Find Real Estate That Suits You.</Heading>
-          <Text fontSize='sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, fugit illo? Delectus, voluptas unde quae cupiditate at amet beatae totam!</Text>
-          <Box py='6'>
-            <Button width='30%'>Get Started</Button>
+      <Stack direction="row" mb='6' overflow='hidden'>
+        <VStack
+          flexGrow='1'
+          px={{ sm: "6", md: "10" }}
+          py={{ sm: '8',  md: "16" }}
+          bg="pink.100"
+          justify="center"
+          align="left"
+          borderRadius="xl"
+        >
+          <Heading fontSize={{ base: "xl", sm: "2xl", md: "3xl" }}>
+            Find Real Estate That Suits You.
+          </Heading>
+          <Text fontSize="sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+            fugit illo? Delectus, voluptas unde quae cupiditate at amet beatae
+            totam!
+          </Text>
+          <Box pt="3" pb="8">
+            <Button>Get Started</Button>
           </Box>
 
-          <HStack spacing='5'>
-            {
-              bannerData.map( (item, index)=>
-                <VStack key={index}>
-                  <HStack>
-                  <Text fontWeight='extrabold'  mr='-2'>{Object.keys(item)}</Text> <BiPlus style={{color: '#ED64A6'}} />
-                  </HStack>
-                  <Text mt='10'>{Object.values(item)}</Text>
-                </VStack>
-              )
-            }
+          <HStack spacing="3">
+            {bannerData.map((item, index) => (
+              <VStack
+                key={index}
+                bg="pink.200"
+                p="4"
+                borderRadius="md"
+                align="left"
+                pr="3"
+              >
+                <HStack>
+                  <Text fontSize={{sm: '14px', md: 'md'}} fontWeight="extrabold" mr="-2">
+                    {Object.keys(item)}
+                  </Text>{" "}
+                  <BiPlus style={{ color: "#ED64A6" }} />
+                </HStack>
+                <Text fontSize={{sm: '12px', md: 'sm'}}>{Object.values(item)}</Text>
+              </VStack>
+            ))}
           </HStack>
         </VStack>
 
-        <SimpleGrid>
-          <GridItem>
-            <Image src={Apartment1Lg} alt='house' />
-          </GridItem>
-          <GridItem>
-            <Image src={Apartment6Lg} alt='house' />
-          </GridItem>
-        </SimpleGrid>
-        {/* <VStack my='10' bg='white'>
-        </VStack> */}
-
-      </Flex>
+        <VStack justify='center'>
+          <Box h='100%' display={{ base: "none", lg: "block", xl:'none' }} >
+            <Image
+              src={Apartment1Lg}
+              alt="house"
+              h='100%'
+              objectFit='cover'
+            />
+          </Box>
+          <Box h='50%' display={{ base: "none", xl: "block" }}>
+            <Image
+              src={Apartment1Lg}
+              alt="house"
+              style={{height: '100%', width: '100%', objectFit: 'contain'}}
+            />
+          </Box>
+          <Box h='50%' display={{ base: "none", xl: "block" }}>
+            <Image
+              src={Apartment6Lg}
+              alt="house"
+              style={{height: '100%', width: '100%', objectFit: 'contain'}}
+            />
+          </Box>
+        </VStack>
+      </Stack>
     </>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
