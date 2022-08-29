b74a0ca095ca004324c7157dbe541ef30966c98e
Anu Kumari <anukumari21529@gmail.com>
2022-08-29 06:24:33 +0530

    update: added location, price & property filter

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,27 +1,25 @@
-import { Button, Flex, Heading, HStack, Select, Stack } from '@chakra-ui/react'
-import React from 'react'
+import { Button, Flex, Heading, VStack, Select, Stack } from '@chakra-ui/react'
+import { useContext} from "react";
+import { HouseContext } from '../../context/HouseContext';
+
+import LocationFilter from "./LocationFilter";
+import PriceFilter from "./PriceFilter";
+import PropertyTypeFilter from "./PropertyTypeFilter";
 
 const Search = () => {
-  return (
-    <Flex direction='column' bg='white' p='4' boxShadow='md'>
-      <Heading py='2' size={{base: 'sm', md: 'md'}}>Search the price you looking for</Heading>
-      <Flex gap={{base: 3, md: 2}} direction={{base: 'column', md:'row'}} borderRadius='30'>
-        <Select placeholder='Location'>
-          <option value='option1'>Option1</option>
-          <option value='option2'>Option2</option>
-        </Select>
-        
-        <Select placeholder='Type'>
-          <option value='option1'>Option1</option>
-          <option value='option2'>Option2</option>
-        </Select>
-        
-        <Select placeholder='Budget'>
-          <option value='option1'>Option1</option>
-          <option value='option2'>Option2</option>
-        </Select>
 
-        <Button p={{base: 3, md: 2}} size="100%">Search</Button>
+  const { searchHandler } = useContext(HouseContext);
+
+  return (
+    <Flex my='3' direction='column' borderRadius='md' bg='#fff' boxShadow='md' p='5'>
+
+      <Heading py='2' size={{base: 'sm', md: 'md'}}>Search the price you looking for</Heading>
+
+      <Flex gap={{base: 3, md: 2}} direction={{base: 'column', md:'row'}} borderRadius='30'>
+        <LocationFilter />
+        <PropertyTypeFilter />
+        <PriceFilter />
+        <Button onClick={searchHandler} p={{base: 3, md: 2}} size="100%">Search</Button>
       </Flex>
     </Flex>
   )
