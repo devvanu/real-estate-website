2a0c444008eb1f3a7ce629d996dfc6c6e42030ec
Anu Kumari <anukumari21529@gmail.com>
2022-08-29 06:33:06 +0530

    update: added HouseContext to HouseList

--- src/components/Houses/HouseList.jsx
+++ src/components/Houses/HouseList.jsx
@@ -1,14 +1,25 @@
-import { Grid, Stack, Image} from '@chakra-ui/react';
+import { Grid, Heading } from '@chakra-ui/react';
+import { useContext } from "react";
+import { HouseContext } from "../../context/HouseContext";
 
-import { housesData } from '../../data';
 import HouseItem from './HouseItem';
 
 const HouseList = () => {
+  const { houses } = useContext(HouseContext);
+
+  if (houses.length === 0) {
+    return (
+      <Heading size="lg" p="10" align="center">
+        Oops... Can try another one?
+      </Heading>
+    );
+  }
+
   return (
     <Grid my='3' rowGap='4' gridTemplateColumns='repeat(auto-fit, minmax(300px, 1fr))' 
     >
       {
-        housesData.map(item=>
+        houses.map(item=>
           <HouseItem key={item.id} house={item} />
         )
       }
